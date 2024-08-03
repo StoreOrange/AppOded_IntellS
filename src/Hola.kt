e7813bca8376429f6d7cb2121c34188ff9d01f98
Orange_Tec <62614494+storeorange@users.noreply.github.com>
2024-08-03 15:40:53 -0600

    Initial commit

---
+++ src/Hola.kt
@@ -0,0 +1,10 @@
+// Will assign kotlin.Unit
+val isUnit = println("es un valor")
+
+fun main() {
+    val temperatura = 10
+    val escaliente = if (temperatura > 38) true else false
+    println(escaliente)
+}
+
+
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,15 @@
-fun main(args: Array<String>){
-    println("Hola Mundo")
+import java.util.*    // required import
+
+
+fun main() {
+    val temperatura = 10
+    val escaliente = if (temperatura > 38) true else false
+    println(escaliente)
 }
 
 
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
